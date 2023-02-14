5481b9ac250d93a7286ec1ad5cc7fbb11e2eac58
Juanchi Taphanel <taphanel.juan@gmail.com>
2023-02-14 09:57:20 -0300

    v1.2

--- desafio-15/server.js
+++ desafio-15/server.js
@@ -205,7 +205,37 @@ app.get('/chat', async (req, res) => {
 
 app.get('/info', routes.getinfo);
 
-app.get('/api/random', routes.getApiRandom);
+//Fork
+app.get(`/api/randoms`, (req, res) => {
+  let msg = 0;
+  req.query.hasOwnProperty("cant")
+    ? (msg = parseInt(req.query.cant))
+    : (msg = 10000);
+  let arrayRandomNum = [];
+  let arrayUsedNumber = [];
+  let arrayRepeatedResult = [];
+  for (let i = 0; i < msg; i++) {
+    arrayRandomNum.push(Math.floor(Math.random() * 1000) + 1);
+  }
+  arrayRandomNum.forEach((num) => {
+    if (!arrayUsedNumber.includes(num)) {
+      arrayUsedNumber.push(num);
+      arrayRepeatedResult.push({
+        [num]: arrayRandomNum.filter((repeatedNum) => repeatedNum == num)
+          .length,
+      });
+    }
+  });
+  res.json({
+    Numeros_generados:
+      "Usted ha generado " +
+      msg +
+      " números. Estos, agrupados por repetición, generaron un array de " +
+      arrayRepeatedResult.length +
+      " elementos",
+    numeros: arrayRepeatedResult,
+  });
+});
 
 app.get("*", (req, res, next) => {
   warnLogger.warn({ metodo: req.method, path: req.path });
